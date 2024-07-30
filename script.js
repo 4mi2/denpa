66acd44e82f965a677c9942d823c287efe42e05a
4mi2 <94160466+4mi2@users.noreply.github.com>
2024-07-30 13:36:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec')
+    fetch('https://script.google.com/macros/s/AKfycbyNKhNtO2q-AKzf139niL7djeJJScPjdAGByKrRWwIzbqijczujkOPX39uGa9arAI4DgQ/exec')
         .then(response => response.json())
         .then(data => {
             document.getElementById('content').innerHTML = data.message;
